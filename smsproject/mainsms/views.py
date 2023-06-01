a0d0929da60423c1782382a128612f1344650d50
Artyom Kalinkin <artyom.artyom.9494@mail.ru>
2023-06-01 22:17:54 +0300

    Создание первых представлений по урл

--- smsproject/mainsms/views.py
+++ smsproject/mainsms/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 
 
+def main(request):
+    return HttpResponse('<h1> HOME page </h1>')
+
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
--- smsproject/smsproject/urls.py
+++ smsproject/smsproject/urls.py
@@ -18,7 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from mainsms.views import index
+
 urlpatterns = [
+    path('', index),
     path('mainsms/', include('mainsms.urls')),
     path('admin/', admin.site.urls),
 ]
