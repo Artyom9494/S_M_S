7e96c3eb87b3bae82ea441e7c5b87111e5652013
Artyom Kalinkin <artyom.artyom.9494@mail.ru>
2023-06-04 19:36:34 +0300

    создание db SqlLite

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## project: _"Store_Management_System"_ or _"S_M_S"_
 
-### steck: Python, Django, HTML, CSS, SQL
+### steck: Python, Django, HTML, CSS, SQLite
 
--- smsproject/mainsms/models.py
+++ smsproject/mainsms/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class MainInfo(models.Model):
+    name_tt = models.CharField(max_length=250)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now_add=True)
