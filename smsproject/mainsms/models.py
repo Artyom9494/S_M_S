115c5d340240acfbc78e395595d99350c3f28d2e
Artyom Kalinkin <artyom.artyom.9494@mail.ru>
2023-06-04 20:18:22 +0300

    миграция данных

--- smsproject/mainsms/models.py
+++ smsproject/mainsms/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class MainInfo(models.Model):
     name_tt = models.CharField(max_length=250)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
--- smsproject/smsproject/settings.py
+++ smsproject/smsproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fq3(&(jefk17-l+a5j@&t2*qsb^!sg@54t#h6(by6hv&n$%hzd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
